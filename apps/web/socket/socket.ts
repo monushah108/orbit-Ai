69f92568545453c5365e6c1f146b87e0eef52533
monushah108 <sonuk627422@gmail.com>
2026-08-05 23:19:58 +0530

    wip:into tailwindcss turborepo

--- apps/web/socket/socket.ts
+++
@@ -1,8 +0,0 @@
-import { io } from "socket.io-client";
-
-const url = (process.env.SOCKET_URL as string) || "http://localhost:8000";
-
-export const socket = io(url, {
-  transports: ["websocket"],
-  autoConnect: false,
-});
