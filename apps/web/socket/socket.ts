3555837d076314fdeefd5b7fcb33c9ce192c09ed
monushah108 <sonuk627422@gmail.com>
2026-08-18 17:00:03 +0530

    change env

--- apps/web/socket/socket.ts
+++ apps/web/socket/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const url = process.env.SOCKET_URL as string;
+const url = process.env.NEXT_PUBLIC_SOCKET_URL as string;
 
 export const socket = io(url, {
   transports: ["websocket"],
